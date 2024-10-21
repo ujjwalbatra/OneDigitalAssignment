296823f81f1f80445e821dfc566fa67b4ac76837
ujjwal.batra <ujjwal.batra@carsales.com.au>
2024-10-22 10:21:47 +1100

    changed console print test

--- tests/test_console_exporter.py
+++ tests/test_console_exporter.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from src.metrics_calculator import Metrics
 from src.console_exporter import ConsoleExporter
@@ -23,11 +24,15 @@ def test_export_metrics_success(capsys, logger):
 
     captured = capsys.readouterr()
 
-    assert "Total Spend: 1000.0" in captured.out
-    assert "Average Purchase: 500.0" in captured.out
-    assert "Max Purchase: 700.0" in captured.out
-    assert "Median Purchase: 500.0" in captured.out
-    assert "Unique Products: 2" in captured.out
+    expected_output = {
+        "total_spend": 1000.0,
+        "avg_purchase": 500.0,
+        "max_purchase": 700.0,
+        "median_purchase": 500.0,
+        "unique_products": 2
+    }
+
+    assert json.loads(captured.out) == expected_output
 
 
 def test_export_metrics_error(monkeypatch, logger):
